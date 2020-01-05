5eb08e2bcb1a0622529cd3f7524fa4b6ce32f033
jozua12345 <jozua12345@outlook.com>
2020-01-05 16:52:20 +0800

    Add hardcoded data

---
+++ Data.js
@@ -0,0 +1,502 @@
+export default const data = {
+    hall1: {
+        name: 'Hall 1',
+        block: [
+            {name: '12', machine: [
+                {name: 'WM120', status: true},
+                {name: 'WM121', status: false},
+                {name: 'WM122', status: true},
+            ]},
+            {name: '13', machine: [
+                {name: 'WM130', status: false},
+                {name: 'WM131', status: false},
+                {name: 'WM132', status: true},
+            ]},
+            {name: '14', machine: [
+                {name: 'WM140', status: true},
+                {name: 'WM141', status: false},
+                {name: 'WM142', status: true},
+            ]},
+            {name: '15', machine: [
+                {name: 'WM150', status: true},
+                {name: 'WM151', status: true},
+                {name: 'WM152', status: true},
+            ]},
+            {name: '16', machine: [
+                {name: 'WM160', status: false},
+                {name: 'WM161', status: false},
+                {name: 'WM162', status: true},
+            ]},
+            {name: '17', machine: [
+                {name: 'WM170', status: true},
+                {name: 'WM171', status: false},
+                {name: 'WM172', status: true},
+            ]},
+            {name: '18', machine: [
+                {name: 'WM180', status: true},
+                {name: 'WM181', status: false},
+                {name: 'WM182', status: true},
+            ]},
+        ]
+    },
+    hall2: {
+        name: 'Hall 2',
+        block: [
+            {name: '1', machine: [
+                {name: 'WM10', status: true},
+                {name: 'WM11', status: false},
+                {name: 'WM12', status: true},
+            ]},
+            {name: '2', machine: [
+                {name: 'WM20', status: false},
+                {name: 'WM21', status: false},
+                {name: 'WM22', status: true},
+            ]},
+            {name: '3', machine: [
+                {name: 'WM30', status: true},
+                {name: 'WM31', status: false},
+                {name: 'WM32', status: true},
+            ]},
+            {name: '4', machine: [
+                {name: 'WM40', status: true},
+                {name: 'WM41', status: true},
+                {name: 'WM42', status: true},
+            ]},
+            {name: '5', machine: [
+                {name: 'WM50', status: false},
+                {name: 'WM51', status: false},
+                {name: 'WM52', status: true},
+            ]},
+            {name: '6', machine: [
+                {name: 'WM60', status: true},
+                {name: 'WM61', status: false},
+                {name: 'WM62', status: true},
+            ]},
+            {name: '7', machine: [
+                {name: 'WM70', status: true},
+                {name: 'WM71', status: false},
+                {name: 'WM72', status: true},
+            ]},
+            {name: '8', machine: [
+                {name: 'WM80', status: true},
+                {name: 'WM81', status: false},
+                {name: 'WM82', status: true},
+            ]},
+            {name: '9', machine: [
+                {name: 'WM90', status: true},
+                {name: 'WM91', status: false},
+                {name: 'WM92', status: true},
+            ]},
+            {name: '10', machine: [
+                {name: 'WM100', status: true},
+                {name: 'WM101', status: false},
+                {name: 'WM102', status: true},
+            ]},
+            {name: '11', machine: [
+                {name: 'WM110', status: true},
+                {name: 'WM111', status: false},
+                {name: 'WM112', status: true},
+            ]},
+        ]
+    },
+    hall3: {
+        name: 'Hall 3',
+        block: [
+            {name: '3A', machine: [
+                {name: 'WM3A0', status: true},
+                {name: 'WM3A1', status: false},
+                {name: 'WM3A2', status: true},
+            ]},
+            {name: '3B', machine: [
+                {name: 'WM3B0', status: false},
+                {name: 'WM3B1', status: false},
+                {name: 'WM3B2', status: true},
+            ]},
+            {name: '3C', machine: [
+                {name: 'WM3C0', status: true},
+                {name: 'WM3C1', status: false},
+                {name: 'WM3C2', status: true},
+            ]},
+            {name: '3D', machine: [
+                {name: 'WM3D0', status: true},
+                {name: 'WM3D1', status: true},
+                {name: 'WM3D2', status: true},
+            ]},
+            {name: '3E', machine: [
+                {name: 'WM3E0', status: false},
+                {name: 'WM3E1', status: false},
+                {name: 'WM3E2', status: true},
+            ]},
+        ]
+    },
+    hall4: {
+        name: 'Hall 4',
+        block: [
+            {name: '22', machine: [
+                {name: 'WM220', status: true},
+                {name: 'WM221', status: false},
+                {name: 'WM222', status: true},
+            ]},
+            {name: '23', machine: [
+                {name: 'WM230', status: false},
+                {name: 'WM231', status: false},
+                {name: 'WM232', status: true},
+            ]},
+            {name: '24', machine: [
+                {name: 'WM240', status: true},
+                {name: 'WM241', status: false},
+                {name: 'WM242', status: true},
+            ]},
+            {name: '25', machine: [
+                {name: 'WM250', status: true},
+                {name: 'WM251', status: true},
+                {name: 'WM252', status: true},
+            ]},
+            {name: '26', machine: [
+                {name: 'WM260', status: false},
+                {name: 'WM261', status: false},
+                {name: 'WM262', status: true},
+            ]},
+            {name: '27', machine: [
+                {name: 'WM270', status: true},
+                {name: 'WM271', status: false},
+                {name: 'WM272', status: true},
+            ]},
+        ]
+    },
+    hall5: {
+        name: 'Hall 5',
+        block: [
+            {name: '28', machine: [
+                {name: 'WM280', status: true},
+                {name: 'WM281', status: false},
+                {name: 'WM282', status: true},
+            ]},
+            {name: '29', machine: [
+                {name: 'WM290', status: false},
+                {name: 'WM291', status: false},
+                {name: 'WM292', status: true},
+            ]},
+            {name: '30', machine: [
+                {name: 'WM300', status: true},
+                {name: 'WM301', status: false},
+                {name: 'WM302', status: true},
+            ]},
+            {name: '31', machine: [
+                {name: 'WM310', status: true},
+                {name: 'WM311', status: true},
+                {name: 'WM312', status: true},
+            ]},
+        ]
+    },
+    hall6: {
+        name: 'Hall 6',
+        block: [
+            {name: '32', machine: [
+                {name: 'WM320', status: true},
+                {name: 'WM321', status: false},
+                {name: 'WM322', status: true},
+            ]},
+            {name: '33', machine: [
+                {name: 'WM330', status: false},
+                {name: 'WM331', status: false},
+                {name: 'WM332', status: true},
+            ]},
+            {name: '34', machine: [
+                {name: 'WM340', status: true},
+                {name: 'WM341', status: false},
+                {name: 'WM342', status: true},
+            ]},
+            {name: '35', machine: [
+                {name: 'WM350', status: true},
+                {name: 'WM351', status: true},
+                {name: 'WM352', status: true},
+            ]},
+        ]
+    },
+    hall7: {
+        name: 'Hall 7',
+        block: [
+            {name: '36', machine: [
+                {name: 'WM360', status: true},
+                {name: 'WM361', status: false},
+                {name: 'WM362', status: true},
+            ]},
+            {name: '37', machine: [
+                {name: 'WM370', status: false},
+                {name: 'WM371', status: false},
+                {name: 'WM372', status: true},
+            ]},
+            {name: '38', machine: [
+                {name: 'WM380', status: true},
+                {name: 'WM381', status: false},
+                {name: 'WM382', status: true},
+            ]},
+            {name: '39', machine: [
+                {name: 'WM390', status: true},
+                {name: 'WM391', status: true},
+                {name: 'WM392', status: true},
+            ]},
+            {name: '40', machine: [
+                {name: 'WM400', status: true},
+                {name: 'WM401', status: true},
+                {name: 'WM402', status: true},
+            ]},
+        ]
+    },
+    hall8: {
+        name: 'Hall 8',
+        block: [
+            {name: '41', machine: [
+                {name: 'WM410', status: true},
+                {name: 'WM411', status: false},
+                {name: 'WM412', status: true},
+            ]},
+            {name: '42', machine: [
+                {name: 'WM420', status: false},
+                {name: 'WM421', status: false},
+                {name: 'WM422', status: true},
+            ]},
+            {name: '43', machine: [
+                {name: 'WM430', status: true},
+                {name: 'WM431', status: false},
+                {name: 'WM432', status: true},
+            ]},
+            {name: '44', machine: [
+                {name: 'WM440', status: true},
+                {name: 'WM441', status: true},
+                {name: 'WM442', status: true},
+            ]},
+        ]
+        
+    },
+    hall9: {
+        name: 'Hall 9',
+        block: [
+            {name: '45', machine: [
+                {name: 'WM450', status: true},
+                {name: 'WM451', status: false},
+                {name: 'WM452', status: true},
+            ]},
+            {name: '46', machine: [
+                {name: 'WM460', status: false},
+                {name: 'WM461', status: false},
+                {name: 'WM462', status: true},
+            ]},
+            {name: '47', machine: [
+                {name: 'WM470', status: true},
+                {name: 'WM471', status: false},
+                {name: 'WM472', status: true},
+            ]},
+            {name: '48', machine: [
+                {name: 'WM480', status: true},
+                {name: 'WM481', status: true},
+                {name: 'WM482', status: true},
+            ]},
+        ]
+    },
+    hall10: {
+        name: 'Hall 10',
+        block: [
+            {name: '49', machine: [
+                {name: 'WM490', status: true},
+                {name: 'WM491', status: false},
+                {name: 'WM492', status: true},
+            ]},
+            {name: '50', machine: [
+                {name: 'WM500', status: false},
+                {name: 'WM501', status: false},
+                {name: 'WM502', status: true},
+            ]},
+            {name: '51', machine: [
+                {name: 'WM510', status: true},
+                {name: 'WM511', status: false},
+                {name: 'WM512', status: true},
+            ]},
+            {name: '52', machine: [
+                {name: 'WM520', status: true},
+                {name: 'WM521', status: true},
+                {name: 'WM522', status: true},
+            ]},
+        ]
+    },
+    hall11: {
+        name: 'Hall 11',
+        block: [
+            {name: '53', machine: [
+                {name: 'WM530', status: true},
+                {name: 'WM531', status: false},
+                {name: 'WM532', status: true},
+            ]},
+            {name: '54', machine: [
+                {name: 'WM540', status: false},
+                {name: 'WM541', status: false},
+                {name: 'WM542', status: true},
+            ]},
+            {name: '55', machine: [
+                {name: 'WM550', status: true},
+                {name: 'WM551', status: false},
+                {name: 'WM552', status: true},
+            ]},
+            {name: '56', machine: [
+                {name: 'WM560', status: true},
+                {name: 'WM561', status: true},
+                {name: 'WM562', status: true},
+            ]},
+        ]
+    },
+    hall12: {
+        name: 'Hall 12',
+        block: [
+            {name: '57', machine: [
+                {name: 'WM570', status: true},
+                {name: 'WM571', status: false},
+                {name: 'WM572', status: true},
+            ]},
+            {name: '58', machine: [
+                {name: 'WM580', status: false},
+                {name: 'WM581', status: false},
+                {name: 'WM582', status: true},
+            ]},
+            {name: '59', machine: [
+                {name: 'WM590', status: true},
+                {name: 'WM591', status: false},
+                {name: 'WM592', status: true},
+            ]},
+            {name: '60', machine: [
+                {name: 'WM600', status: true},
+                {name: 'WM601', status: true},
+                {name: 'WM602', status: true},
+            ]},
+        ]
+    },
+    hall13: {
+        name: 'Hall 13',
+        block: [
+            {name: '61', machine: [
+                {name: 'WM610', status: true},
+                {name: 'WM611', status: false},
+                {name: 'WM612', status: true},
+            ]},
+            {name: '62', machine: [
+                {name: 'WM620', status: false},
+                {name: 'WM621', status: false},
+                {name: 'WM622', status: true},
+            ]},
+            {name: '63', machine: [
+                {name: 'WM630', status: true},
+                {name: 'WM631', status: false},
+                {name: 'WM632', status: true},
+            ]},
+            {name: '64', machine: [
+                {name: 'WM640', status: true},
+                {name: 'WM641', status: true},
+                {name: 'WM642', status: true},
+            ]},
+        ]
+    },
+    hall14: {
+        name: 'Hall 14',
+        block: [
+            {name: '65', machine: [
+                {name: 'WM650', status: true},
+                {name: 'WM651', status: false},
+                {name: 'WM652', status: true},
+            ]},
+            {name: '66', machine: [
+                {name: 'WM660', status: false},
+                {name: 'WM661', status: false},
+                {name: 'WM662', status: true},
+            ]},
+            {name: '67', machine: [
+                {name: 'WM670', status: true},
+                {name: 'WM671', status: false},
+                {name: 'WM672', status: true},
+            ]},
+            {name: '68', machine: [
+                {name: 'WM680', status: true},
+                {name: 'WM681', status: true},
+                {name: 'WM682', status: true},
+            ]},
+        ]
+    },
+    hall15: {
+        name: 'Hall 15',
+        block: [
+            {name: '69', machine: [
+                {name: 'WM690', status: true},
+                {name: 'WM691', status: false},
+                {name: 'WM692', status: true},
+            ]},
+            {name: '70', machine: [
+                {name: 'WM700', status: false},
+                {name: 'WM701', status: false},
+                {name: 'WM702', status: true},
+            ]},
+            {name: '71', machine: [
+                {name: 'WM710', status: true},
+                {name: 'WM711', status: false},
+                {name: 'WM712', status: true},
+            ]},
+            {name: '72', machine: [
+                {name: 'WM720', status: true},
+                {name: 'WM721', status: true},
+                {name: 'WM722', status: true},
+            ]},
+        ]
+    },
+    hall16: {
+        name: 'Hall 16',
+        block: [
+            {name: '16A', machine: [
+                {name: 'WM16A0', status: true},
+                {name: 'WM16A1', status: false},
+                {name: 'WM16A2', status: true},
+            ]},
+            {name: '16B', machine: [
+                {name: 'WM16B0', status: false},
+                {name: 'WM16B1', status: false},
+                {name: 'WM16B2', status: true},
+            ]},
+            {name: '16C', machine: [
+                {name: 'WM16C0', status: true},
+                {name: 'WM16C1', status: false},
+                {name: 'WM16C2', status: true},
+            ]},
+            {name: '16D', machine: [
+                {name: 'WM16D0', status: true},
+                {name: 'WM16D1', status: true},
+                {name: 'WM16D2', status: true},
+            ]},
+            {name: '16E', machine: [
+                {name: 'WM16E0', status: false},
+                {name: 'WM16E1', status: false},
+                {name: 'WM16E2', status: true},
+            ]},
+        ]
+    },
+    crescent: {
+        name: 'Crescent',
+    },
+    pioneer: {
+        name: 'Pioneer',
+    },
+    binjai: {
+        name: 'Binjai',
+    },
+    tanjong: {
+        name: 'Tanjong',
+    },
+    banyan: {
+        name: 'Banyan',
+    },
+    saraca: {
+        name: 'Saraca',
+    },
+    tamarind: {
+        name: 'Tamarind',
+    },
+    meranti: {
+        name: 'Meranti',
+    }
+}
